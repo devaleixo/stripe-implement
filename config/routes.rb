78ffd077f5399d9953502474edd8fdb103efc055
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2023-09-05 08:01:49 -0300

    added controllers and routes for teddies

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users
+  root 'teddies#index'
+  resources :teddies, only: [:index, :show]
 end
