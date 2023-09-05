03aef53c6fed9adb301c604a327de0f37214ffbe
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2023-09-05 14:11:47 -0300

    added price to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,7 @@ geek = Category.create!(name: 'geek')
 kids = Category.create!(name: 'kids')
 
 puts 'Creating teddies...'
-Teddy.create!( name: 'Teddy bear', category: kids, photo_url: 'http://onehdwallpaper.com/wp-content/uploads/2015/07/Teddy-Bears-HD-Images.jpg')
-
-Teddy.create!( name: 'Jean-Michel - Le Wagon', category: geek, photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
-Teddy.create!(  name: 'Octocat -  GitHub',      category: geek, photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
+Teddy.create!(price: 80, name: 'Teddy bear', category: kids, photo_url: 'http://onehdwallpaper.com/wp-content/uploads/2015/07/Teddy-Bears-HD-Images.jpg')
+Teddy.create!( price_cents: 9000, name: 'Jean-Michel - Le Wagon', category: geek, photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
+Teddy.create!(  price_cents: 850,name: 'Octocat -  GitHub',      category: geek, photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
 puts 'Finished!'
