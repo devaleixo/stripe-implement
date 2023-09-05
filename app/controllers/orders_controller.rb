f692328a3aec328b4fcc20ae5f41c9621ab92407
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2023-09-05 14:59:12 -0300

    added orders controller logic for stripe and button to redirect to orders/new in teddie form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,23 @@ class OrdersController < ApplicationController
   end
 
   def create
+    teddy = Teddy.find(params[:teddy_id])
+    order  = Order.create!(teddy: teddy, amount: teddy.price, state: 'pending', user: current_user)
+
+  session = Stripe::Checkout::Session.create(
+    payment_method_types: ['card', 'pix'],
+    line_items: [{
+      name: teddy.sku,
+      images: [teddy.photo_url],
+      amount: teddy.price_cents,
+      currency: 'brl',
+      quantity: 1
+    }],
+    success_url: order_url(order),
+    cancel_url: order_url(order)
+  )
+
+  order.update(checkout_session_id: session.id)
+  redirect_to new_order_payment_path(order)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'orders/show'
-  get 'orders/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'teddies#index'
+  resources :orders, only: [:show, :create]
   resources :teddies, only: [:index, :show]
 end
