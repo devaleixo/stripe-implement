ea3929e01937e410277865273c866c67dbcff5ac
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2023-09-05 08:15:46 -0300

    added devise gem and set it up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  devise_for :users
   root 'teddies#index'
   resources :teddies, only: [:index, :show]
 end
